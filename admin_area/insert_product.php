253eaf80dc43644dea56f6e1cfd6056e12d666d1
RameshNc <rameshnc355@gmail.com>
2017-09-02 22:47:39 +0530

    ecommerce

---
+++ admin_area/insert_product.php
@@ -0,0 +1,76 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Inserting product</title>
+</head>
+<body>
+
+<form action="insert_product.php" method="post" enctype="multipart/form-data">
+    
+    <h2 align="center">Insert New Product Here</h2> 
+    
+    <table align="center" width="750" border="2" bgcolor="pink">
+        
+           
+    
+        <tr>
+            <td align="right"><b>Product title:</b></td>
+            <td><input type="text" name="product_title"/></td>
+        
+         </tr>
+        
+        <tr>
+            <td align="right"><b>Product category:</b></td>
+            <td><input type="text" name="product_title"/></td>
+        
+         </tr>
+        
+        <tr>
+            <td align="right"><b>Product brand:</b></td>
+            <td><input type="text" name="product_title"/></td>
+        
+         </tr>
+        
+        <tr>
+            <td align="right"><b>Product image:</b></td>
+            <td><input type="text" name="product_title"/></td>
+        
+         </tr>
+        
+        <tr>
+            <td align="right"><b>Product price:</b></td>
+            <td><input type="text" name="product_title"/></td>
+        
+         </tr>
+        
+        <tr>
+            <td align="right"><b>Product description:</b></td>
+            <td><input type="text" name="product_title"/></td>
+        
+         </tr>
+        
+        <tr>
+            <td align="right"><b>Product keyword:</b></td>
+            <td><input type="text" name="product_title"/></td>
+        
+         </tr>
+        
+        <tr align="center">
+            <td colspan="7"><input type="submit" name="insert_post" value="Insert Now"/></td>
+        
+         </tr>
+    
+    
+    
+    
+    </table>
+    
+    </form>
+
+
+
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>my online shop</title>
+
+<link rel="stylesheet" href="styles/style.css" media="all"/>
+</head>
+<body>
+
+<div class="main">
+    
+    <div class="header">
+    
+    <img id="logo" src="images/logo.png"/>
+    <img id="name" src="images/name.png"/>
+    
+    
+    </div>
+    
+    <div class="menubar">
+        <ul id="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">All Products</a></li>
+            <li><a href="#">My Account</a></li>
+            <li><a href="#">Sign Up</a></li>
+            <li><a href="#">Shopping Cart</a></li>
+            <li><a href="#">Contact Us</a></li>
+        
+        
+        
+        
+        
+        
+        </ul>
+    
+    <div id="form">
+        <form method="get" action="results.php" enctype="multipart/form-data">
+        <input type="text" name="user_query" placeholder="Search a product" />
+        <input type="submit" name="search" value="Search" />    
+        
+        </form>
+        
+        
+        </div>
+    
+    
+    </div>
+    
+    <div class="content">
+    
+    <div id="sidebar">
+        <div id="sidebar_title">Categories</div>
+        
+        <ul id="cats">
+            <li><a href="#">Laptops</a></li>
+            <li><a href="#">Computers</a></li>
+            <li><a href="#">Mobiles</a></li>
+            <li><a href="#">Cameras</a></li>
+            <li><a href="#">iPads</a></li>
+            <li><a href="#">Tablets</a></li>
+        
+        
+        
+        </ul>
+        
+        
+        <div id="sidebar_title">Brands</div>
+        
+        <ul id="cats">
+            <li><a href="#">HP</a></li>
+            <li><a href="#">DELL</a></li>
+            <li><a href="#">Motorola</a></li>
+            <li><a href="#">Sony Eracson</a></li>
+            <li><a href="#">LG</a></li>
+            <li><a href="#">Apple</a></li>
+        
+        
+        
+        </ul>
+        
+    </div>
+    
+    <div id="content">This is content area</div>
+        
+    </div>
+    
+    <div id="footer">This is footer</div>
+    
+    
+    
+    
+    
+    
+    
+    
+</div>
+
+
+
+
+</body>
+</html>
